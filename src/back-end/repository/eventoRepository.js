89a391cc26d803e029711213edf85816a9a81d6a
Marcos Vinicius Cavalcante Rosa <marcosviniciuscrosa@gmail.com>
2023-10-18 10:54:13 -0300

    dois querys

--- src/back-end/repository/eventoRepository.js
+++ src/back-end/repository/eventoRepository.js
@@ -1,12 +1,36 @@
 import { connection } from "./connection.js";
 
 
-export async function listar() {
+export async function listarTodos() {
   const comando = `
      SELECT *
        FROM evento
   `
-
   const [linhas] = await connection.query(comando);
+  return linhas;
+}
+
+export async function salvar(evento) {
+  const comando = `
+      INSERT INTO eventos (nome, espaco, endereco,
+      cep, capacidademaxima, data, horario)
+      VALUES (?, ?, ?, ?, ?, ?, ?)
+  `
+
+  const [info] = await connection.query(comando,
+  [evento.nome, evento.espaco, evento.endereco, evento.cep, evento.capacidademaxima])
+  evento.id = info.insertId;
+  
+  return evento;
+}
+
+export async function buscarPorNome(nome) {
+  const comando = `
+    SELECT nome, espaco, endereco, cep, capacidademaxima, data, horario
+    FROM tb_filme
+    WHERE nome like ?
+  `
+
+  const [linhas] = await connection.query(comando, ['%'+nome+'%']);
   return linhas;
 }
