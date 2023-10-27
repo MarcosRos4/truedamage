57aa964c3df1b68a4ae459629350ae5d52c9a4bb
Marcos Vinicius Cavalcante Rosa <marcosviniciuscrosa@gmail.com>
2023-10-27 11:16:56 -0300

    responsividade login resolvida definitivo

--- src/login/Login.js
+++ src/login/Login.js
@@ -3,12 +3,17 @@ import './Login.scss'
 import faixagiants from "../images/faixa giants.png"
 import truedamagelogo from "../images/true damage logo.png"
 import truedamagegravacao from "../images/true damage gavacao.jpg"
+import truedamagegravacaotablet from "../images/true damage gavacao tablet.png"
 
 
 export default function Login(){
     return(
-        <div className="Login">        
-            <img src={truedamagegravacao} alt="Gravação True Damage"></img>
+        <div className="Login">   
+            
+            <img src={truedamagegravacaotablet} alt="tablet" className='imgtablet'></img>
+            <img src={truedamagegravacao} alt="banda true damage" className='imgdesktop'></img>
+            
+            
             <div class="form-div">
                 <img class="giants1" src={faixagiants} alt="Faixa Giants"></img>
                 <form class="form" action="">
