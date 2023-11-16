46630a48fe650f2fa0a65f814d72e8bc3c9e5f50
Marcos Vinicius Cavalcante Rosa <marcosviniciuscrosa@gmail.com>
2023-11-16 09:56:34 -0300

    preparação pra subir no azure

--- src/api/repository/connection.js
+++ src/api/repository/connection.js
@@ -1,10 +1,10 @@
 import mysql2 from 'mysql2/promise'
 
 const connection = await mysql2.createConnection({
-    host: process.env.HOSTDB,
-    database: process.env.DB,
-    user: process.env.USER,
-    password: process.env.PASSWORD
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DB,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
 })
 
 console.log("Conexão com o BD realizada")
